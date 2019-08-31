d1f4475317ada77d1792d635c56fad0c9b457926
JR <1336913542@qq.com>
2019-08-31 17:46:24 +0800

    IU-2018.3.4 <jr@JR-2.local Create baseRefactoring.xml, markdown.xml, vim_settings.xml, ide.general.xml, project.default.xml, lombok-plugin.xml, other.xml, yiiguxing.translation.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, webDeployTransportHostTargets.xml, path.macros.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, javarebel.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, laf.xml, MyBatisCodeHelper.xml, vcs.xml, editor.codeinsight.xml, toStringTemplates.xml, keymap.xml, editor.xml, debugger.xml, Mac OS X copy.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+ /**
+ * @Author JR
+ * @Description TODO 
+ * @Date ${DATE} ${TIME}
+ * @Version 1.0
+ **/
+public class ${NAME} {
+}
